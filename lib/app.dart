9e67956a16b11e18cc6da4b95d4ec5bc83e20201
kaipo-hu <kaipo.hu@fazzfinancial.com>
2023-03-31 11:08:53 +0800

    change main.dart and widget_test.dart

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+import 'routes/app_router.gr.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
+final routeObserver = AutoRouteObserver();
+final appRouter = AppRouter();
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      routeInformationProvider: appRouter.routeInfoProvider(),
+      routeInformationParser: appRouter.defaultRouteParser(),
+      routerDelegate: appRouter.delegate(
+        initialRoutes: [const HomeRoute()],
+        navigatorObservers: () => [routeObserver],
+      ),
+    );
+  }
+}
