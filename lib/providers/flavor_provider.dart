f563a196706241407e1e327df681ca6328088b37
kaipohu <kaipohu520@gmail.com>
2023-04-06 21:33:10 +0800

    add bootstrap and apply flavor

---
+++ lib/providers/flavor_provider.dart
@@ -0,0 +1,5 @@
+part of 'providers.dart';
+
+final flavorProvider = StateProvider<Flavor>(
+  (ref) => throw UnimplementedError('set value using override'),
+);
---
+++ lib/providers/providers.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../config/flavor.dart';
+
+part 'flavor_provider.dart';
