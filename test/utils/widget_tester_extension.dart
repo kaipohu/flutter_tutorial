ecd48c832f0a5a675ab518a28964f07ff8fe2388
kaipohu <kaipohu520@gmail.com>
2023-04-06 21:35:26 +0800

    update test_app

--- test/utils/widget_tester_extension.dart
+++ test/utils/widget_tester_extension.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'test_app.dart';
@@ -7,11 +8,13 @@ import 'test_app.dart';
 extension WidgetTesterExtension on WidgetTester {
   Future<void> pumpWidgetInTestApp(
     Widget widget, {
+    List<Override> overrides = const [],
     StackRouter? stackRouter,
   }) {
     return pumpWidget(
       TestApp(
         stackRouter: stackRouter,
+        overrides: overrides,
         child: widget,
       ),
     );
